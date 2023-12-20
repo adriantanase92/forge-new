ef70fc6acd59a74a6d886659881e4e7aed68c0f9
Adrian Tanase <tanase.adrian92@gmail.com>
2023-12-20 14:01:05 +0200

    further implementations

---
+++ packages/forge-main-client/src/lib/shared/components/modules/roles/Role.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { colors, type Role } from '$lib/shared';
+	import Button from '../../general/button/Button.svelte';
+
+	export let role: Role;
+</script>
+
+{#if role}
+	<div
+		class="rounded-xl group p-6 shadow-sm border border-rhino hover:border-cobalt border-solid flex justify-between items-center"
+	>
+		<div class="capitalize text-xl font-semibold group-hover:text-cobalt">
+			{role.name}
+		</div>
+		<div class="flex gap-2">
+			<Button
+				class="p-2"
+				kind="icon"
+				color="rhino"
+				icon="pencil"
+				iconHeight="18"
+				iconWidth="18"
+				iconColor={colors.white}
+				on:click={() => console.log(`edit role ${role.id}`)}
+			/>
+			<Button
+				class="p-2"
+				kind="icon"
+				color="error"
+				icon="bin"
+				iconHeight="18"
+				iconWidth="18"
+				iconColor={colors.white}
+				on:click={() => console.log(`delete role ${role.id}`)}
+			/>
+		</div>
+	</div>
+{/if}
--- packages/forge-main-client/src/routes/[lang=locale]/(protected)/roles/+page.svelte
+++ packages/forge-main-client/src/routes/[lang=locale]/(protected)/roles/+page.svelte
@@ -5,6 +5,7 @@
 	import { capitalize, colors } from '$lib/shared/index.js';
 	import LL from '$i18n/i18n-svelte';
 	import Button from '$lib/shared/components/general/button/Button.svelte';
+	import Role from '$lib/shared/components/modules/roles/Role.svelte';
 
 	export let data;
 
@@ -31,41 +32,9 @@
 		</Button>
 	</div>
 
-	<DynamicDataRenderer
-		layout="grid"
-		gap="gap-6"
-		gridClasses="grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-	>
+	<DynamicDataRenderer layout="list" gap="gap-6">
 		{#each roles as role}
-			<div
-				class="rounded-xl group p-6 shadow-sm border border-rhino hover:border-cobalt border-solid flex justify-between items-center"
-			>
-				<div class="capitalize text-xl font-semibold group-hover:text-cobalt">
-					{role.name}
-				</div>
-				<div class="flex gap-2">
-					<Button
-						class="p-2"
-						kind="icon"
-						color="rhino"
-						icon="pencil"
-						iconHeight="18"
-						iconWidth="18"
-						iconColor={colors.white}
-						on:click={() => console.log(`edit role ${role.id}`)}
-					/>
-					<Button
-						class="p-2"
-						kind="icon"
-						color="error"
-						icon="bin"
-						iconHeight="18"
-						iconWidth="18"
-						iconColor={colors.white}
-						on:click={() => console.log(`delete role ${role.id}`)}
-					/>
-				</div>
-			</div>
+			<Role {role} />
 		{:else}
 			<p>{$LL.errors.noSomethingFound({ something: $LL.modules.roles.entity.multiple() })}</p>
 		{/each}
